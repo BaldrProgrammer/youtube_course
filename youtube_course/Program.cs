1038ad67c48731347b39ebfa5b69e254ef565c85
balddrAdmin <boliklevik@gmail.com>
2025-11-09 00:55:54 +0100

    types of data conversion

--- youtube_course/Program.cs
+++ youtube_course/Program.cs
@@ -1,11 +1,16 @@
-﻿int age = 25;
-double height = 1.75;
-string name = "Alice";
+﻿// явное преобразование
+float floatnum = 10.5f;
+int intnum = (int)floatnum;
+Console.WriteLine(intnum);
+
+
+// неявное преобразование
+int num = 123;
+double doublenum = num;
+Console.WriteLine(doublenum);
+    
+    
+// вроде явное, хз
 bool isStudent = true;
-
-float floatHeight = 1.75f;
-
-Console.WriteLine("Name: " + name);
-Console.WriteLine("Age" + age);
-Console.WriteLine("Height: " + height + "or in float" + floatHeight);
-Console.WriteLine("Student? " + isStudent + ".");
+int isStudentInt = Convert.ToInt32(isStudent); // нельзя с бул переменную запихнуть инт
+string isStudentStr = Convert.ToString(isStudentInt); // та же схема 
