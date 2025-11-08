59d7afd469b47bfd62d4dfb9a1cf1ed7ebc13fb3
balddrAdmin <boliklevik@gmail.com>
2025-11-09 00:40:55 +0100

    variables | types of data

---
+++ youtube_course/Program.cs
@@ -0,0 +1,11 @@
+﻿int age = 25;
+double height = 1.75;
+string name = "Alice";
+bool isStudent = true;
+
+float floatHeight = 1.75f;
+
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Age" + age);
+Console.WriteLine("Height: " + height + "or in float" + floatHeight);
+Console.WriteLine("Student? " + isStudent + ".");
